4dde7793c283ff32b7d9b887703b006267b7227c
tuha <tuha263@gmail.com>
2024-05-14 09:47:49 +0700

    Fix can't create setting file

--- Assets/Src/ByteBrewSDK/Editor/ByteBrewManagerEditor.cs
+++ Assets/Src/ByteBrewSDK/Editor/ByteBrewManagerEditor.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -11,6 +9,11 @@ namespace ByteBrewSDK
     [CustomEditor(typeof(ByteBrewSettings))]
     public class ByteBrewEditor : Editor
     {
+        public ByteBrewEditor()
+        {
+            ByteBrewSettingsManager.EnsureByteBrewSettings();
+        }
+        
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
